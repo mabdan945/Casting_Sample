91becbc01fbcfd5284d6c2067ad0d7cbfe02db22
taufiiqurahman <taufiiqurahman@gmail.comgit config --global user.name taufiiqurahman>
2024-06-01 19:54:16 +0700

    update

---
+++ pages/1_⚙️_ℹ️How_To Use.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+markdown = """
+Web App URL: <https://capstoneproject37.streamlit.app/>
+GitHub Repository: <https://github.com/TaufiiquRahman/CapstoneProject37>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+
+st.title("How To Use")
+
+
