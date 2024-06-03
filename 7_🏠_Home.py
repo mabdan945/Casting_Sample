e1b34fec9cf47963e03fd4f3e6ee2ab7633ca188
mabdan945 <120587880+mabdan945@users.noreply.github.com>
2024-06-03 11:10:44 +0700

    Update 7_🏠_Home.py

--- 7_🏠_Home.py
+++ 7_🏠_Home.py
@@ -2,65 +2,75 @@ import streamlit as st
 from keras.models import load_model
 from PIL import Image
 import numpy as np
-import yaml
-from yaml.loader import SafeLoader
-
-with open('/config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-
 from util import classify, set_background
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
+# Set background
+set_background('./bgrd/bg.jpg')
+
+# Define CSS for the title, header, image name, and text boxes
+st.markdown(
+    """
+    <style>
+    .title-box, .header-box, .filename-box, .box {
+        border: 1px solid #000;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: #333;
+        color: white;
+        margin-top: 20px;
+    }
+    .title-box {
+        text-align: center;
+        font-size: 32px;
+        font-weight: bold;
+    }
+    .header-box {
+        text-align: center;
+        font-size: 24px;
+        font-weight: bold;
+    }
+    .filename-box {
+        text-align: center;
+        font-size: 18px;
+    }
+    .box h2, .box h3 {
+        margin: 0;
+    }
+    </style>
+    """, unsafe_allow_html=True
 )
 
-authenticator.login()
+# Set title
+st.markdown('<div class="title-box">Casting Quality Control</div>', unsafe_allow_html=True)
 
-if st.session_state["authentication_status"]:
-    authenticator.logout()
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
+# Set header
+st.markdown('<div class="header-box">Please upload a Casting Product Image</div>', unsafe_allow_html=True)
 
+# Upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
+# Load classifier
+model = load_model('./modelcast.h5')
 
-    set_background('./bgrd/bg.jpg')
+# Load class names
+with open('./model/label.txt', 'r') as f:
+    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
 
-    # set title
-    st.title('Casting Quality Control')
+# Display image and classification results
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+    
+    # Display image file name
+    st.markdown(f'<div class="filename-box">Uploaded file: {file.name}</div>', unsafe_allow_html=True)
 
-    # set header
-    st.header('Please upload a Casting Product Image')
+    # Classify image
+    class_name, conf_score = classify(image, model, class_names)
 
-    # upload file
-    file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
-
-
-    # load classifier
-    model = load_model('./modelcast.h5')
-
-    # load class names
-    with open('./model/label.txt', 'r') as f:
-        class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
-        f.close()
-
-
-    # display image
-    if file is not None:
-        image = Image.open(file).convert('RGB')
-        st.image(image, use_column_width=True)
-
-        # classify image
-        class_name, conf_score = classify(image, model, class_names)
-
-        # write classification
-        st.write("## {}".format(class_name))
-        st.write("### score: {}%".format(int(conf_score * 1000) / 10))
+    # Write classification in a box
+    st.markdown(f"""
+    <div class="box">
+        <h2>{class_name}</h2>
+        <h3>score: {int(conf_score * 1000) / 10}%</h3>
+    </div>
+    """, unsafe_allow_html=True)
